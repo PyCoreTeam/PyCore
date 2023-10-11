b609c7facf03ec07285a2a5c2e81c4e5b421c546
ltzXiaoYanMo <xiajiaruimail@qq.com>
2023-10-11 20:47:47 +0800

    头要大了tmd

--- PyCore.HackTool.GUI.py
+++ PyCore.HackTool.GUI.py
@@ -1,8 +1,8 @@
 import _thread
 import os
 import sys
-import tkinter as tk
 
+import tkinter as tk
 from PyCoreHackToolUtils import *
 
 ## 此处参考无GUI的注释
@@ -69,11 +69,12 @@ def main():
                print(f"Error:{e}")
                pass
 
+
 # 创建窗口
 def create_window():
    window = tk.Tk()
    window.title("PyCore SBFLSQ")  # 标题
-   window.geometry("400x300")     # 分辨率
+   window.geometry("600x400")     # 分辨率
    window.configure(bg="white")   # 背景颜色
 
    # 首信息
@@ -82,23 +83,38 @@ def create_window():
 
    # 副消息
    submit_button = tk.Label(window,text="傻鸟FLSQ，建议重开", font=("微软雅黑", 16), bg="white")
-   submit_button.pack(pady=20)
+   submit_button.pack(pady=10)
+   
+   # 退出按钮
+   submit_button = tk.Button(window, text="Exit", font=("Arial", 8), command=window.quit)
+   submit_button(pady=5)
 
    entry = tk.Entry(window, font=("Arial", 16))
    entry.pack(pady=10)
-
+   
    def on_submit():
        cmd = entry.get()
        entry.delete(0, tk.END)
        if cmd:
            print(cmd)
            main()
-   
+
+    # sshatk按钮
+   submit_button = tk.Button(window, text="SSH攻击", font=("微软雅黑", 8), bg="white", command=lambda: sshAttack(entry.get(), int(entry.get()), int(entry.get())))
+   submit_button.pack(pady=3)
+
+   # httpatk按钮
+   submit_button = tk.Button(window, text="HTTP攻击", font=("微软雅黑", 8), bg="white",command=lambda: tcpAttack(entry.get(), int(entry.get()), int(entry.get())))
+   submit_button.pack(pady=3)
+   window.mainloop()
+
    # 启动按钮
    submit_button = tk.Button(window, text="攻击FLSQ垃圾服务器", font=("微软雅黑", 10), command=on_submit)
    submit_button.pack(pady=10)
 
-   window.mainloop()
 
-if __name__ == "__main__":
+# 奇怪的运行方式（
+if __name__ == main:
    create_window()
+else:
+    main()
